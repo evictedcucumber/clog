4b2eb0b1eef438ba675dfd63c61805920c1bdfc0
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-05-12 19:04:04 +0000

    feat: switched to using macros for options

--- src/clog.h
+++ src/clog.h
@@ -1,31 +1,21 @@
-#ifndef CLOGGER_H
-#define CLOGGER_H
+#ifndef CLOG_H
+#define CLOG_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct {
-    int enable_colours;
-    int enable_timestamps;
-    char* timestamp_format;
-} ClogLogger;
-
-typedef enum { Debug, Info, Warn, Error, Critical } ClogLogLevel;
-
-ClogLogger* clog_get_logger(void);
-
-void clog_log_msg(const char* msg, ClogLogLevel level);
+void clog_log_msg(const char*, int level);
 
 #ifdef CLOG_ENABLE_DEBUG
-#define clog_log_debug(msg) clog_log_msg(msg, Debug);
+#define clog_log_debug(msg) clog_log_msg(msg, 1);
 #else
 #define clog_log_debug(msg) (void)0
 #endif
-#define clog_log_info(msg) clog_log_msg(msg, Info);
-#define clog_log_warn(msg) clog_log_msg(msg, Warn);
-#define clog_log_error(msg) clog_log_msg(msg, Error);
-#define clog_log_critical(msg) clog_log_msg(msg, Critical);
+#define clog_log_info(msg) clog_log_msg(msg, 2);
+#define clog_log_warn(msg) clog_log_msg(msg, 3);
+#define clog_log_error(msg) clog_log_msg(msg, 4);
+#define clog_log_critical(msg) clog_log_msg(msg, 5);
 
 #ifdef __cplusplus
 }
@@ -35,10 +25,6 @@ void clog_log_msg(const char* msg, ClogLogLevel level);
 #define CLOG_PREFIX_GUARD
 #ifndef CLOG_ENABLE_PREFIX
 
-#define Logger ClogLogger
-#define LogLevel ClogLogLevel
-
-#define get_logger clog_get_logger
 #define log_msg clog_log_msg
 
 #define log_debug clog_log_debug
